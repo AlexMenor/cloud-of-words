a868a7bf2a4467f3aba327f1db5f3ec6a82d3307
Alex <alex4menor@gmail.com>
2019-11-16 20:07:14 +0100

    getcloudcomponent working

--- client/src/components/GetCloud.vue
+++ client/src/components/GetCloud.vue
@@ -1,21 +1,52 @@
 <template>
   <div class="contentContainer">
-    <ul>
-      <li v-for="(phrase, i) in phrases" :key="i">{{phrase}}</li>
-    </ul>
+    <vue-word-cloud
+      :words="wordsSorted"
+      :color="
+        ([, weight]) =>
+          weight > 10 ? 'DeepPink' : weight > 5 ? 'RoyalBlue' : 'Indigo'
+      "
+      font-family="Roboto"
+    />
   </div>
 </template>
 
 <script>
 import socket from "../socketio";
+import VueWordCloud from "vuewordcloud";
 export default {
+  components: { [VueWordCloud.name]: VueWordCloud },
   data: () => ({
-    phrases: []
+    words: new Map(),
+    wordsSorted: [],
+    maxWords: 10
   }),
   mounted() {
-    socket.on("WORDS", words => {
-      this.phrases.push(words);
+    socket.on("WORDS", newWords => {
+      this.recalculate(newWords.split(" "));
     });
+  },
+  methods: {
+    recalculate(arrayOfNewWords) {
+      const newMap = new Map(this.words);
+      arrayOfNewWords.forEach(word => {
+        const currentValue = newMap.get(word);
+        if (currentValue) newMap.set(word, currentValue + 1);
+        else newMap.set(word, 1);
+      });
+      this.words = newMap;
+    }
+  },
+  watch: {
+    words: function(val) {
+      const toReturn = [];
+      val.forEach((key, value) => {
+        toReturn.push([value, key]);
+      });
+      this.wordsSorted = toReturn
+        .sort(([, a], [, b]) => (b < a ? -1 : b > a ? 1 : 0))
+        .slice(0, this.maxWords);
+    }
   }
 };
 </script>
