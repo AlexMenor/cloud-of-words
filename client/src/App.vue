909b1cc13b5e132af7a5e3de6d9be86b338884ca
Alex Menor <alexmenor@github.com>
2019-11-18 16:50:37 +0100

    visual feedback for clickables

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark src="./assets/toolbar-background.png">
-      <v-img
-        class="clickable"
-        @click="$router.push('/')"
-        id="logo"
-        src="./assets/logo.png"
-        max-height="70px"
-        max-width="70px"
-      ></v-img>
-      <v-toolbar-title
-        @click="$router.push('/')"
-        class="clickable headline font-weight-light"
-      >Cloud Of Words</v-toolbar-title>
+      <div class="clickable" @click="$router.push('/')">
+        <v-img id="logo" src="./assets/logo.png" max-height="70px" max-width="70px"></v-img>
+        <v-toolbar-title
+          @click="$router.push('/')"
+          class="clickable headline font-weight-light"
+        >Cloud Of Words</v-toolbar-title>
+      </div>
       <v-spacer></v-spacer>
       <v-toolbar-title class="hideIfMobile">{{ context }}</v-toolbar-title>
-      <v-btn icon href="http://github.com/AlexMenor/cloud-of-words" target="_blank">
+      <v-btn
+        class="clickable"
+        icon
+        href="http://github.com/AlexMenor/cloud-of-words"
+        target="_blank"
+      >
         <v-icon large>mdi-github-circle</v-icon>
       </v-btn>
     </v-app-bar>
@@ -49,5 +49,10 @@ export default {
 }
 .clickable {
   cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+.clickable:hover {
+  opacity: 0.6;
 }
 </style>
