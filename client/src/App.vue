32fd490d23006e2c0df574a7d99c4f11566de5b3
Alex Menor <alexmenor@github.com>
2019-11-18 16:34:36 +0100

    logo in navbar is now a button home

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,21 +2,20 @@
   <v-app>
     <v-app-bar app color="primary" dark src="./assets/toolbar-background.png">
       <v-img
+        class="clickable"
+        @click="$router.push('/')"
         id="logo"
         src="./assets/logo.png"
         max-height="70px"
         max-width="70px"
       ></v-img>
-      <v-toolbar-title class="headline font-weight-light"
-        >Cloud Of Words</v-toolbar-title
-      >
+      <v-toolbar-title
+        @click="$router.push('/')"
+        class="clickable headline font-weight-light"
+      >Cloud Of Words</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-title class="hideIfMobile">{{ context }}</v-toolbar-title>
-      <v-btn
-        icon
-        href="http://github.com/AlexMenor/cloud-of-words"
-        target="_blank"
-      >
+      <v-btn icon href="http://github.com/AlexMenor/cloud-of-words" target="_blank">
         <v-icon large>mdi-github-circle</v-icon>
       </v-btn>
     </v-app-bar>
@@ -47,5 +46,8 @@ export default {
   .hideIfMobile {
     display: none;
   }
+}
+.clickable {
+  cursor: pointer;
 }
 </style>
